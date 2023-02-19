b37dc4c97661da859910bc5358c778d3ba8cf56b
Mohamed Arafath <113768609+MohamedArafath205@users.noreply.github.com>
2023-02-19 14:11:27 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import openai
+
+
+st.set_page_config(page_title="Chat GPT", page_icon=":tada:", layout="wide")
+
+# ---- Header ----
+
+st.subheader("""
+            HI :wave:, 
+            I am Mohamed Arafath an AIML student at SRM Institue of Science and Technology. 
+            I am a Jack of all cards! 
+            I know Machine Learning and Intrested to learn more about web developement""")
+st.title("This is my [GitHub](https://github.com/MohamedArafath205) follow me!")
+
+title = st.text_input("Ask me anything...")
+
+
+openai.api_key = "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"
+
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=title,
+  temperature=0,
+  max_tokens=60,
+  top_p=1,
+  frequency_penalty=0.5,
+  presence_penalty=0
+)
+if st.button('Enter'):
+    st.success(response.choices[0].text)
